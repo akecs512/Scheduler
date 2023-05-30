e98fc4b706152fd5f2d6c00ef065c75062f53333
Adrienne Kecskes <akecs619@yahoo.com>
2023-05-29 20:32:07 -0500

    add block colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,23 @@
 
 $("#currentDay").html(dayjs().format("dddd MM/DD/YYYY hh:mm a"))
-
-
-$(function () {
-
-});
+var currentHour = dayjs().hour()
 var startHour = 9
-var endHour = 17
+var endHour = 23
 var timeArray = []
 
 
+
 for (let index = startHour; index < endHour; index++) {
+  let color = "past"
   let hour = index
+  if (hour === currentHour) {
+    color = "present"
+  } else if (hour > currentHour) {
+    color = "future"
+  }
+
   let timeBlockEl = `
-<div id="hour-9" class="row time-block past">
+<div id="hour-9" class="row time-block ${color}">
 <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
 <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
 <button class="btn saveBtn col-2 col-md-1" aria-label="save">
