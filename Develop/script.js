1999414e70377aa517cba1822b800b7680043f65
Adrienne Kecskes <akecs619@yahoo.com>
2023-05-29 21:12:39 -0500

    save text area to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,12 @@ var startHour = 9
 var endHour = 23
 var timeArray = []
 
-
+$(document).on("click", ".saveBtn", function (event) {
+  let timeBlockId = event.currentTarget.parentElement.id;
+  console.log(timeBlockId)
+  localStorage.setItem(timeBlockId, $(this).siblings('textarea').val())
+}
+);
 
 for (let index = startHour; index < endHour; index++) {
   let color = "past"
@@ -15,19 +20,18 @@ for (let index = startHour; index < endHour; index++) {
   } else if (hour > currentHour) {
     color = "future"
   }
+  let text = localStorage.getItem("hour-" + hour) || ""
 
   let timeBlockEl = `
-<div id="hour-9" class="row time-block ${color}">
-<div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
-<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  <i class="fas fa-save" aria-hidden="true"></i>
-</button>
+<div id="hour-${hour}" class="row time-block ${color}">
+  <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
+  <textarea class="col-8 col-md-10 description" rows="3">${text}</textarea>
+  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+   <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
 </div>`
 
   timeArray.push(timeBlockEl)
-
 }
-
 
 $(".container-fluid").html(timeArray)
